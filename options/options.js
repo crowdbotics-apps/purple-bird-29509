07c3a920af2bb8aa546ece754992f1eec6d3af9b
Crowdbotics <team@crowdbotics.com>
2021-08-05 14:28:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "purple_bird_29509Identifier",
+  url: "https://purple_bird_29509Identifier.botics.co",
+  api: "https://purple_bird_29509Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
